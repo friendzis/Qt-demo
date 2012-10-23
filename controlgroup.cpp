7088058f8478d90b37b2e2e36d60a9c2550e888a
friendzis <me@friendzis.net>
2012-10-23 09:33:41 +0300

    ControlGroup: added some comments

--- controlgroup.cpp
+++ controlgroup.cpp
@@ -1,6 +1,8 @@
 #include "controlgroup.h"
 
 ControlGroup::ControlGroup()
+// QObject::tr() handles translations. Now it is sufficient to provide translations file
+// and this string can be automagically tranlated. Kewl!
     : QGroupBox(tr("Application control"))
 {
 }
