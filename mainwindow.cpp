b96e1a5f6852e17be22bc9ff1948e90958be1317
friendzis <me@friendzis.net>
2012-10-23 09:28:17 +0300

    MainWindow: Added comments

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,13 +1,22 @@
 #include "mainwindow.h"
 
+// Constructors are always called when a class is instantiated
+// A class can have multiple overloaded constructors
+// A constructor that has no input parameters (or all parameters have default values)
+// is called the default constructor. There shalt be one default constructor per class
 MainWindow::MainWindow(QWidget *parent)
+// Here we are calling parent class constructor. The beauty of polymorphism
     : QWidget(parent)
 {
     QVBoxLayout * mainLayout = new QVBoxLayout();
+    // Here we are calling the default constructor, therefore () are not necessary
     this->m_controlGroup = new ControlGroup;
 
+    // Here we add widgets to holding layout. Most visual containers do not accept bare
+    // widgets not enlosed in layouts
     mainLayout->addWidget(this->m_controlGroup);
 
+    // Our fresh baked layout is now set as the contents of our main window
     this->setLayout(mainLayout);
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,10 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+// Some headers are not necessarily needed as they might be included in other headers.
+// But explicitly including all the required headers minimises the risk of error
+// and makes it obvious what components class is dependant on.
+
 #include <QWidget>
 
 #include <QVBoxLayout>
@@ -8,12 +12,21 @@
 
 class MainWindow : public QWidget
 {
+    // This macro enables Qt functionality (Qt's meta-system)
+    // It requires that calling class inherits from QObject
+    // (not necessarily directly)
     Q_OBJECT
     
+// Public class members (methods/properties) are accessible (callable) from anywhere in
+// in the code (provided you have access to an actual object)
 public:
+    // This is the default constructor
     MainWindow(QWidget *parent = 0);
+    //  The destructor
     ~MainWindow();
 
+// Private class members are accessible from any instance of the same class, not only
+// the holding instance
 private:
     ControlGroup * m_controlGroup;
 };
