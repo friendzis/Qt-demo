81fc3765d7a18e0ea3bfba2536adea868ee98073
friendzis <me@friendzis.net>
2012-10-23 08:50:45 +0300

    main.cpp: added some extended comments

--- main.cpp
+++ main.cpp
@@ -3,9 +3,15 @@
 
 int main(int argc, char *argv[])
 {
+    // QApplication is basically an outer event loop that handles links between
+    // system/application events and event handlers. It is also responsible for redrawing
+    // with all the consequences
     QApplication a(argc, argv);
+
     MainWindow w;
+    // The application will happily run without this. Just without any visual interface ;]
     w.show();
     
+    // This starts the event loop
     return a.exec();
 }
