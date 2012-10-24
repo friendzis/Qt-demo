c7959e8e7ddb4cf09cfdf7ca8b3c9eb885ea376c
friendzis <me@friendzis.net>
2012-10-24 08:58:21 +0300

    ControlGroup: *created* some buttons. Note that these buttons actually exist, but need explicit visual representation

--- controlgroup.cpp
+++ controlgroup.cpp
@@ -5,4 +5,14 @@ ControlGroup::ControlGroup()
 // and this string can be automagically tranlated. Kewl!
     : QGroupBox(tr("Application control"))
 {
+    // Qt allows to assign shortcuts (alt+symbol) to clickable objects
+    // by preceding a symbol with &. The assignment is handled automagically. Convenient :)
+    this->m_startButton = new QPushButton(tr("&Start"));
+
+    // Without explicitly providing class scope the compiler first tries to take
+    // variable of same name in function scope. If that fails - tries more global scope
+    // which is class scope. Then goes namespaces, etc.
+    m_stopButton = new QPushButton(tr("S&top"));
+
+    this->m_clearButton = new QPushButton(tr("&Clear"));
 }
--- controlgroup.h
+++ controlgroup.h
@@ -2,11 +2,17 @@
 #define CONTROLGROUP_H
 
 #include <QGroupBox>
+#include <QPushButton>
 
 class ControlGroup : public QGroupBox
 {
 public:
     ControlGroup();
+
+private:
+    QPushButton * m_startButton;
+    QPushButton * m_stopButton;
+    QPushButton * m_clearButton;
 };
 
 #endif // CONTROLGROUP_H
